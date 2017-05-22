4481af5a7cf22bf7b1ae31df51cbaf3735900e0f
ilyademyankov <mail@ialexeev.ru>
2017-05-23 00:52:39 +0300

    Create model_main .php

---
+++ application/models/model_main .php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ilya
+ * Date: 23.05.17
+ * Time: 0:17
+ */
+class ModelMain extends Model
+{
+    function GetMenu(){
+      return "this is menu!";
+    }
+
+}
